719d0175167993fbcf8c6c21c98df025e857e3d3
RyanMaugin <ryanmauginn@gmail.com>
2016-11-14 00:02:50 +0000

    Finished Main View UI

--- Weatherly/WeatherViewController.swift
+++ Weatherly/WeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherViewController.swift
 //  Weatherly
 //
 //  Created by Ryan Maugin on 12/11/2016.
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
