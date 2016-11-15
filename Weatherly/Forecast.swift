3efc517a68e34340511526ce0df19b329566b402
RyanMaugin <ryanmauginn@gmail.com>
2016-11-15 00:58:29 +0000

    Added Forecast class variables and encapsulation for the variables

--- Weatherly/Forecast.swift
+++ Weatherly/Forecast.swift
@@ -11,4 +11,40 @@ import Alamofire
 
 class Forecast {
     
+    //// Variables
+    var _date: String!
+    var _weatherType: String!
+    var _highTemp: String!
+    var _lowTemp: String!
+    
+    //// Data encapsulation
+    
+    var date: String {
+        if _date == nil {
+            _date = ""
+        }
+        return _date
+    }
+    
+    var weatherType: String {
+        if _weatherType == nil {
+            _weatherType = ""
+        }
+        return _weatherType
+    }
+    
+    var highTemp: String {
+        if _highTemp == nil {
+            _highTemp = ""
+        }
+        return _highTemp
+    }
+    
+    var lowTemp: String {
+        if _lowTemp == nil {
+            _lowTemp = ""
+        }
+        return _lowTemp
+    }
+    
 }
