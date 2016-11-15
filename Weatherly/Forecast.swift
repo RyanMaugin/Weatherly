03bc2d05c4917e7f62202914a9cc7cee1f4afa3d
RyanMaugin <ryanmauginn@gmail.com>
2016-11-15 00:47:50 +0000

    Created Forecast file and class + fixed some constraints in main view

---
+++ Weatherly/Forecast.swift
@@ -0,0 +1,14 @@
+//
+//  Forecast.swift
+//  Weatherly
+//
+//  Created by Ryan Maugin on 15/11/2016.
+//  Copyright © 2016 MauginApps. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class Forecast {
+    
+}
