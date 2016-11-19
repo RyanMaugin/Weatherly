d6a6090f0fd2a174b5f7cdedf663145e8c7f6b07
RyanMaugin <ryanmauginn@gmail.com>
2016-11-19 18:58:01 +0000

    Created Weatherell.swift file for forecast weather cell

---
+++ Weatherly/WeatherCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherCell.swift
+//  Weatherly
+//
+//  Created by Ryan Maugin on 19/11/2016.
+//  Copyright © 2016 MauginApps. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
