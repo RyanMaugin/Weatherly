eebc765781e465e503229e44e960122af17ec546
RyanMaugin <ryanmauginn@gmail.com>
2016-12-02 23:47:28 +0000

    Created DayCell cocoa touch class

---
+++ Weatherly/DayCell.swift
@@ -0,0 +1,15 @@
+//
+//  DayCell.swift
+//  Weatherly
+//
+//  Created by Ryan Maugin on 02/12/2016.
+//  Copyright © 2016 MauginApps. All rights reserved.
+//
+
+import UIKit
+
+class DayCell: UITableViewCell {
+
+    
+
+}
