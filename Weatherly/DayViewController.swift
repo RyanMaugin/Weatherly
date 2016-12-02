716f7282480c55b15b72dce1c8f83189db2e6bb5
RyanMaugin <ryanmauginn@gmail.com>
2016-12-02 00:21:31 +0000

    Added test graph view with desired theme

--- Weatherly/DayViewController.swift
+++ Weatherly/DayViewController.swift
@@ -7,23 +7,33 @@
 //
 
 import UIKit
+import ScrollableGraphView
 
 class DayViewController: UIViewController {
     
-    //// Passed data from WeatherViewController
+    /// Passed data from WeatherViewController
     private var _passedDayIndex: String!
     var passedDayIndex: String {
         get { return _passedDayIndex } set { _passedDayIndex = newValue }
     }
     
-    //// Variables
+    /// Variables
     var dayForecast: DayForecast!
-
+    
+    /// Graph View Variables
+    @IBOutlet weak var graphView: ScrollableGraphView?
+    var graphLabels: [String] = ["ONE", "TWO", "THREE", "FOUR"]
+    var graphData: [Double] = [10.0, 8.0, 24.0, 36.0]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // This will run when the view is loaded and will 
         dayForecast = DayForecast(dayIndex: Int(_passedDayIndex)!)
+        
+        // Graph view test
+        graphView?.lineStyle = ScrollableGraphViewLineStyle.smooth
+        graphView?.set(data: graphData, withLabels: graphLabels)
     }
     
     override func viewDidAppear(_ animated: Bool) {
